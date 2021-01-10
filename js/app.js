863687ec1d632ed5863fa5fdcbb3217abc22adcb
Loayelshall <Loay.elshall@icloud.com>
2021-01-10 16:52:06 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -35,6 +35,8 @@ let sections = document.getElementsByTagName('section')
  * 
 */
 
+// Add class 'active' to section when near top of viewport
+
 // build the nav
 let navList = document.getElementById('navBar')
 for(let section of sections){
@@ -45,37 +47,48 @@ for(let section of sections){
     newUrl.setAttribute('href',`#${section.id}`)
     newLink.classList = "nav-item"
     newUrl.classList = "nav-link"
+    newUrl.setAttribute('id',`${section.id}Link`)
     newLink.appendChild(newUrl)
     navList.appendChild(newLink)
     
 }
 const navBar = document.querySelector('nav')
-const stickyDistance = navBar.offsetTop
 
-function navBarResponsive(){
-    console.log(window.pageYOffset)
-    if(window.pageYOffset >= stickyDistance){
-        navBar.classList.add("sticky")
-        console.log(window.pageYOffset)
-    } else {
-        navBar.classList.remove("sticky")
-        console.log('there')
-    }
-} 
-window.onscroll = navBarResponsive()
 document.addEventListener('scroll',function(){
-    if(window.pageYOffset >= stickyDistance){
+    if(window.pageYOffset >= 60){
         navBar.classList.add("sticky")
-        
     } else {
         navBar.classList.remove("sticky")
-        console.log('there')
-    }})
+    }
 
-// Add class 'active' to section when near top of viewport
+    
+    for (section of sections){
+        const sectionOffset = section.getBoundingClientRect()
+        if(100 > sectionOffset.top && sectionOffset.top > -50){
+            const link = document.getElementById(`${section.id}Link`)
+            link.classList.add("active")  
+            section.classList.add("activeSection")
+            section.style.setProperty("--scroll",window.pageYOffset/(document.body.offsetHeight-window.innerHeight))
+        } else {
+            const link = document.getElementById(`${section.id}Link`)
+            link.classList.remove("active")
+            // section.classList.remove("activeSection")
+            section.style.setProperty("--scroll",0)
+        }
+        
+    }
+    
+    
+
+})
+
+
+
+
 
 
 // Scroll to anchor ID using scrollTO event
+
 
 
 /**
