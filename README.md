84c1251812c81ff3fcd02b778c5dd2227ea1266e
Loayelshall <Loay.elshall@icloud.com>
2021-01-16 23:50:38 +0200

    Comments update

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
 
 ## Table of Contents
 
+* [How to run the project](#Howtoruntheproject)
 * [How it works](#Howitworks)
+* [Dependencies](#Dependencies)
+* [Refrences](#Refrences)
+
 
 ## 
 
+
+## How to run the project
+1) Clone the repo [How to Clone a repo](https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository) or download and extract the project 
+2) If you have a code editor(e.g. VSCode), you can open the repo`s folder and right click on (index.html) and choose (Open with live server)
+3) If you dont have a code editor you can go ahead and double click on (index.html) and it should take you right to the page in your default browser of choice (note: Website may not be fully functional) 
 
 ## How it works
   
@@ -13,3 +22,11 @@
 2) Then i implmented a function that loops through current page sections and build the navbar accordingly 
 3) then i added the event lisenter ( to listen for scrolling event ) to detect the current position of the window and the relative positions of the navbar and page sections to change their state from active to not active and vice versa when they are currently viewed
 4) Also used the scroll to function to smoothly scroll the user to the desired section when the corresponding button is pressed
+
+## Dependencies 
+This project does not have any dependencies, However please make sure to use a modern browser for full functionality 
+
+## Refrences 
+1) [W3Schools](https://www.w3schools.com/)
+2) [StackOverflow](https://stackoverflow.com/)
+3) [Udacity Web Development Proffessional NanoDegree](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011)
--- js/app.js
+++ js/app.js
@@ -32,8 +32,34 @@ let sections = document.getElementsByTagName('section')
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+*/ 
 
+
+// Adding sections dynamically 
+function createSections(n, paragraphs){
+    const main = document.querySelector('main')
+    for (let i=1; i<=n; i++){
+        const newSection = document.createElement('section')
+        newSection.setAttribute('id',`section${i}`)
+        newSection.setAttribute('data-nav',`Section ${i}`)
+
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('landing__container')
+
+        const newHTag = document.createElement('h2')
+        newHTag.innerHTML = `Section ${i}`
+        const newp = document.createElement('p')
+        newp.innerHTML = paragraphs[i][1]
+        const newp2 = document.createElement('p')
+        newp2.innerHTML = paragraphs[i][2]
+
+        newDiv.appendChild(newHTag)
+        newDiv.appendChild(newp)
+        newDiv.appendChild(newp2)
+        newSection.appendChild(newDiv)
+        main.appendChild(newSection)
+    }
+}
 
 // build the navbar elements by dynamically adding the links according to the sections in the homepage
 function buildNav(){
@@ -63,7 +89,7 @@ let lastSize = 0
 
 // Using the scroll event listener to detect the current position of the window and change the class of the section currently in view as well as its link in the navbar
 // to active state to state which section is currently active
-document.addEventListener('scroll',function(){
+document.addEventListener('scroll',()=>{
     // adding and removing the sticky class to the navbar according to the position of the screen 
     if(window.pageYOffset >= 10){
         navBar.classList.add("sticky")
@@ -81,13 +107,27 @@ document.addEventListener('scroll',function(){
             let st = window.pageYOffset
             if(st>lastScrollTop){
                 //down
-                lastSize = (-sectionOffset.top/1000)*2+1.75 > 1.65 ? 1.65:((-sectionOffset.top/1000)*2+1.75 < 1.2 ? 1.2:(-sectionOffset.top/1000)*2+1.75) 
-                section.style.fontSize = `${lastSize}rem`
+                // detecting screen width to resize the font for better readability on phones
+                if(screen.width < 400){
+                    lastSize = (-sectionOffset.top/1000)*2+1.75 > 1.4 ? 1.4:((-sectionOffset.top/1000)*2+1.75 < 1 ? 1:(-sectionOffset.top/1000)*2+1.75) 
+                    section.style.fontSize = `${lastSize}rem`
+                } else {
+                    lastSize = (-sectionOffset.top/1000)*2+1.75 > 1.65 ? 1.65:((-sectionOffset.top/1000)*2+1.75 < 1.2 ? 1.2:(-sectionOffset.top/1000)*2+1.75) 
+                    section.style.fontSize = `${lastSize}rem`
+                }
+                
                 
             } else {
                 //up
-                size = lastSize - (sectionOffset.top/1000) > 1.65 ? 1.65: (lastSize - (sectionOffset.top/1000) < 1.2 ? 1.2 : lastSize - (sectionOffset.top/1000))
-                section.style.fontSize = `${size}rem`
+                // detecting screen width to resize the font for better readability on phones
+                if(screen.width < 400){
+                    size = lastSize - (sectionOffset.top/1000) > 1.4 ? 1.4: (lastSize - (sectionOffset.top/1000) < 1 ? 1 : lastSize - (sectionOffset.top/1000))
+                    section.style.fontSize = `${size}rem`
+                } else {
+                    size = lastSize - (sectionOffset.top/1000) > 1.65 ? 1.65: (lastSize - (sectionOffset.top/1000) < 1.2 ? 1.2 : lastSize - (sectionOffset.top/1000))
+                    section.style.fontSize = `${size}rem`
+                }
+                
             }
             lastScrollTop = st<=0 ? 0:st
  
@@ -131,12 +171,33 @@ function goToSection(id){
  * 
 */
 
+
+// Add sections to the page 
+// You can change n and the paragraphs added to the page to your liking
+const n = 4
+// You can dynamically change sections text by changing this json or even update its data dynamically through an api
+let paragraphs = {
+    '1': {
+        '1' :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+        '2' : "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.",
+    },
+
+    '2': {
+        '1' :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+        '2' : "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.",
+    },
+    '3': {
+        '1' :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+        '2' : "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.",
+    },
+    '4': {
+        '1' :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+        '2' : "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.",
+    }
+}
+createSections(n,paragraphs)
 // Build menu 
 buildNav()
 
-// Scroll to section on link click
-
-
-// Set sections as active
 
 
